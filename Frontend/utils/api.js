79977cb9670b29be87c5d47c9193d7733ceebeb4
Parth Sidana <tanishsidana123@gmail.com>
2026-02-04 13:59:12 +0530

    fixed deployement

--- Frontend/utils/api.js
+++ Frontend/utils/api.js
@@ -1,58 +1,6 @@
-// import axios from 'axios';
-
-// const API_URL = import.meta.env.VITE_API_URL;
-
-// // Create axios instance
-// const api = axios.create({
-//   baseURL: API_URL,
-//   headers: {
-//     'Content-Type': 'application/json'
-//   }
-// });
-
-// // Add token to requests
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// // Handle response errors
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem('token');
-//       localStorage.removeItem('user');
-//       window.location.href = '/login';
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
-
-
-
-
-
-
-
-
-
-
-
-
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Create axios instance
 const api = axios.create({
@@ -81,7 +29,6 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      // Token expired or invalid
       localStorage.removeItem('token');
       localStorage.removeItem('user');
       window.location.href = '/login';
@@ -91,6 +38,59 @@ api.interceptors.response.use(
 );
 
 export default api;
+
+
+
+
+
+
+
+//locally
+
+
+
+
+// import axios from 'axios';
+
+// const API_URL = 'http://localhost:5000/api';
+
+// // Create axios instance
+// const api = axios.create({
+//   baseURL: API_URL,
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// });
+
+// // Add token to requests
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// // Handle response errors
+// api.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       // Token expired or invalid
+//       localStorage.removeItem('token');
+//       localStorage.removeItem('user');
+//       window.location.href = '/login';
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default api;
 
 
 
